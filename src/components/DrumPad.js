7a60aad459af571c0dc5144471f2c07347c5c38e
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-12-25 13:21:15 +0800

    Play DrumPad audio when clicking/pressing the key

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const DrumPad = ({ sound }) => (
-  <div className="drum-pad" id={sound.id}>
-    <audio className="clip" src={sound.url} id={sound.key} />
-    {sound.key}
-  </div>
-);
+class DrumPad extends Component {
+  static propTypes = {
+    sound: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      keyCode: PropTypes.number.isRequired,
+      key: PropTypes.string.isRequired
+    })
+  };
 
-DrumPad.propTypes = {
-  sound: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    keyCode: PropTypes.number.isRequired,
-    key: PropTypes.string.isRequired
-  })
-};
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    if (e.keyCode === this.props.sound.keyCode) {
+      this.playSound();
+    }
+  };
+
+  playSound = () => {
+    const sound = document.getElementById(this.props.sound.key);
+    if (!sound) {
+      return;
+    }
+    sound.currentTime = 0;
+    sound.play();
+  };
+
+  render() {
+    const { sound } = this.props;
+    return (
+      <div className="drum-pad" id={sound.id} onClick={this.playSound}>
+        <audio className="clip" src={sound.url} id={sound.key} />
+        {sound.key}
+      </div>
+    );
+  }
+}
 
 export default DrumPad;
