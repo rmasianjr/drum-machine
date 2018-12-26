cd02cbb30d0af1e0ecb10b7f4264155e46e3ea3e
Ricardo Masian Jr <ricardo.masianjr@gmail.com>
2018-12-26 18:03:41 +0800

    Updates

---
+++ precache-manifest.ad00cc48f0797931fe89d49de60f467d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "8fdec94c536cd2307996",
+    "url": "/drum-machine/static/js/main.8fdec94c.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/drum-machine/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "8fdec94c536cd2307996",
+    "url": "/drum-machine/static/css/main.e8606df9.chunk.css"
+  },
+  {
+    "revision": "ad759d4d3da39d3780351b1630e2c693",
+    "url": "/drum-machine/index.html"
+  }
+];
